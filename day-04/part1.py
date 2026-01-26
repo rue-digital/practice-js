8589772710534e2b2a955a9b00fe01e1da2a9576
Arunima Tripathy <rue@mightyacorn.com>
2026-01-26 11:14:30 -0500

    day 4: calculate number of cells with less than 4 neighbors

---
+++ day-04/part1.py
@@ -0,0 +1,41 @@
+filepath = "example.txt"
+
+with open(filepath, "r") as file:
+    grid = [line.strip("\n") for line in file]
+
+directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1]
+]
+
+rows = len(grid)
+cols = len(grid[0])
+
+total = 0
+
+for row in range(rows):
+    for col in range(cols):
+
+        adjacentCount = 0
+
+        if grid[row][col] == ".":
+            continue
+
+        for d in directions:
+            neighborX = row + d[0]
+            neighborY = col + d[1]
+
+            if 0 <= neighborX < rows and 0 <= neighborY < cols and grid[neighborX][neighborY]=="@":
+                adjacentCount+=1
+        
+        if adjacentCount < 4:
+            total +=1
+
+assert total == 13, f"expected 13, got {total}"
+print(f"total: {total}")
